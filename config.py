658941ef0d1b20ef30a935a0b70115e69d74ff54
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-04 19:07:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,6 +28,7 @@ START_STICKER_ID = getenv("START_STICKER_ID", "CAACAgUAAxkBAAEL5olmGPWEXFq5JVQtv
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Idhaya X Music")
 
+POWERED_BY = getenv("POWERED_BY", "𝐈𝐃𝐇𝐀𝐘𝐀𝐍ᥫ᭡")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
